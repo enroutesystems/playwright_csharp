ca94f4858b4ba4ba356875f4603a5ca08bb5ca81
Cesar Rivera <carc@live.com.mx>
2024-10-07 10:45:05 -0600

    make allure cli installation optional

--- Utils/Helpers.cs
+++ Utils/Helpers.cs
@@ -10,11 +10,42 @@ public class Helpers
         string[] possibleStrings = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"];
         return Array.IndexOf(possibleStrings, sequence);
     }
+    private static bool IsAllureInstalled(string allureCommand)
+    {
+        try
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = allureCommand,
+                    Arguments = "--version",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            var error = process.StandardError.ReadToEnd();
+            return process.ExitCode == 0 && string.IsNullOrEmpty(error);
+        }
+        catch
+        {
+            return false;
+        }
+    }
     public static Task GenerateAllureReport()
     {
         var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-        var projectRoot = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
         var allureCommand = isWindows ? "allure.exe" : "allure";
+        var projectRoot = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+        if (!IsAllureInstalled(allureCommand))
+        {
+            Console.WriteLine("Allure is not installed. Please install Allure CLI to generate the report.");
+            return Task.CompletedTask;
+        }
         var process = new Process
         {
             StartInfo = new ProcessStartInfo
